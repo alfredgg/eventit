9199878263b30add3d9575df664b0019e25f26eb
Alfred <alfredgg@yahoo.es>
2016-07-23 20:49:24 +0200

    Add event model

--- data_model.py
+++ data_model.py
@@ -1,3 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+
+from app import db
+from datetime import datetime
+from slugify import slugify
+
+
+class Event (db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120))
+    slug = db.Column(db.String, nullable=False, unique=True)
+    description = db.Column(db.String())
+    created = db.Column(db.DateTime, default=datetime.utcnow)
+    updated = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __init__(self, name):
+        self.name = name
+        self.slug = slugify(name)
