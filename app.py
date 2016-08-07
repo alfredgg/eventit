cf69ee9a40c31ccc530eee3b20fbef2a45250564
Alfred <alfredgg@yahoo.es>
2016-08-07 11:10:04 +0200

    Remove unnecessary files

--- app.py
+++ app.py
@@ -10,5 +10,5 @@ app = Flask(__name__)
 if os.path.exists(os.path.join(os.path.dirname(__file__), 'app_config.py')):
     app.config.from_object('app_config')
 
-app.config.from_envvar('BACKEND_CONFIG_MODULE', silent=True)
+app.config.from_envvar('EVENTIT_CONFIG_MODULE', silent=True)
 db = SQLAlchemy(app)
