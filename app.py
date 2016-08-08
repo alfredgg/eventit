372863ea795c6ef16056d4a510d589428f07fcde
Alfred <alfredgg@yahoo.es>
2016-08-08 11:58:20 +0200

    Add the possibility to change the static folder

--- app.py
+++ app.py
@@ -12,6 +12,10 @@ if os.path.exists(os.path.join(os.path.dirname(__file__), 'app_config.py')):
     app.config.from_object('app_config')
 
 app.config.from_envvar('EVENTIT_CONFIG_MODULE', silent=True)
+
+if app.config['STATIC_FOLDER']:
+    app.static_folder = app.config['STATIC_FOLDER']
+
 db = SQLAlchemy(app)
 
 login_manager = LoginManager()
